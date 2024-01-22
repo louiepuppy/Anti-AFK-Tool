60b3092dd822dae61c0125d543bc71f161cb2f70
louie <154765282+louiepuppy@users.noreply.github.com>
2024-01-21 22:10:26 -0600

    Squashed a bug.
    Fixed "keyboard_input" not being updated when the button was pressed to turn it on/off.
    Also refactored the code a bit.

--- afk.py
+++ afk.py
@@ -3,53 +3,72 @@ import random
 import time
 import pyautogui
 
-def keyboardPress():
+
+# Main program that creates a window, formats widgets, and adds clickable buttons
+def main():    
+    ## create window and declare some global variables
+    global window
+    window = tk.Tk()
+    window.geometry("250x250")
+    window.title("Anti-AFK Tool")
+    global keyboard_input
+    keyboard_input = tk.BooleanVar()
+
+    ## window widgets
+    startButton = tk.Button(window, text = "Start", command = start_func, width = 6, height = 1, font = ("Helvetica", 16, "bold"))
+    stopButton = tk.Button(window, text = "Stop", command = stop_func, width = 6, height = 1, font = ("Helvetica", 16, "bold"))
+    global status
+    status = tk.Label(window, text = "", font = ("Helvetica", 12, "italic"), width = 16)
+    keyboardButton = tk.Checkbutton(window, text="Random key toggle", variable=keyboard_input, font=("Helvetica", 12))
+    label = tk.Label(window, text = "made by louie", font = ("Helvetica", 8, "italic", "bold"))
+
+    ## formatting
+    startButton.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
+    stopButton.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
+    keyboardButton.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
+    status.pack()
+    label.pack(side=tk.BOTTOM, padx=10, pady=10)
+    
+    ## window waits for input
+    window.mainloop()
+
+
+# Function to handle pressing keys
+def keyboard_press():
+    global keyboard_input
     keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
             'w', 'x', 'y', 'z']
     key = random.choice(keys)
-    if keyboard_input.get(): # only press if randKey is enabled
+    if keyboard_input.get(): # only press if keyboard_input is enabled
         pyautogui.press(key)
 
+
+# Function to handle mouse movement and calling the keyboard_press function
 def nudge():
     global running
     if running:
         dx = random.randint(-5,5) # picks random number to nudge mouse random pixels in x direction
         dy = random.randint(-5,5) # now in y direction
         pyautogui.move(dx,dy)
-        keyboardPress() # randomly press a key
+        keyboard_press() # randomly press a key
         window.after(random.randint(5,20)*1000, nudge)
 
-def startFunc():
+
+# Function to start the program and init the nudge function
+def start_func():
     global running
     running = True
     status.config(text = "Program initiated.")
     nudge()
 
-def stopFunc():
+
+# Function that stops the nudge and keyboard_press functions
+def stop_func():
     global running
     running = False
     status.config(text = "Program paused.")
 
 
-## create window
-window = tk.Tk()
-window.geometry("250x250")
-window.title("Anti-AFK")
-keyboard_input = tk.BooleanVar()
-
-## window widgets
-startButton = tk.Button(window, text = "Start", command = startFunc, width = 6, height = 1, font = ("Helvetica", 16, "bold"))
-stopButton = tk.Button(window, text = "Stop", command = stopFunc, width = 6, height = 1, font = ("Helvetica", 16, "bold"))
-status = tk.Label(window, text = "", font = ("Helvetica", 12, "italic"), width = 16)
-keyboardButton = tk.Checkbutton(window, text="Random key toggle", variable=keyboard_input, font=("Helvetica", 12))
-label = tk.Label(window, text = "made by louie", font = ("Helvetica", 8, "italic", "bold"))
-
-## formatting
-startButton.pack(side = tk.TOP, fill = tk.X, padx = 10, pady = 10)
-stopButton.pack(side = tk.TOP, fill = tk.X, padx = 10, pady = 10)
-keyboardButton.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
-status.pack()
-label.pack(side = tk.BOTTOM, padx = 10, pady = 10)
-
-
-window.mainloop()
+# Init program
+if __name__ == "__main__":
+    main()
